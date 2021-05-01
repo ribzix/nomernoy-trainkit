8e693e2317d55316b7575ab2dd8e85f15a10ba7f
ribzix <37005911+ribzix@users.noreply.github.com>
2021-05-01 17:10:59 +0200

    Creation of the trainkit structure

---
+++ Train/Cabin/MainPanel/VelocityFormula.lua
@@ -0,0 +1,9 @@
+while true do
+	local part = script.Parent
+	local speed = part.Velocity.magnitude
+	wait(0.1)
+	speed = speed * 1.003
+	speed = (math.abs(speed))
+	speed = (math.floor(speed))
+	script.V.Value = speed
+end
