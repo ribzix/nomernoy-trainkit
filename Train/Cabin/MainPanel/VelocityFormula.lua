760a6fee8c327ba22a46f14e6554916036d9662c
ribzix <37005911+ribzix@users.noreply.github.com>
2021-06-11 20:21:58 +0200

    Delete Train/Cabin/MainPanel directory

--- Train/Cabin/MainPanel/VelocityFormula.lua
+++
@@ -1,9 +0,0 @@
-while true do
-	local part = script.Parent
-	local speed = part.Velocity.magnitude
-	wait(0.1)
-	speed = speed * 1.003
-	speed = (math.abs(speed))
-	speed = (math.floor(speed))
-	script.V.Value = speed
-end
