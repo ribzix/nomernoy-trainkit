69139ef710d2dbc5ffa219e6fe2481edf2daf00b
ribzix <37005911+ribzix@users.noreply.github.com>
2021-10-17 19:56:16 +0300

    Added a way to play sounds without affecting the performance of the trainkit

---
+++ SoundManager.lua
@@ -0,0 +1,32 @@
+local train = workspace.BogeyTest
+local trainA = workspace.BogeyONE
+local train1 = workspace.TrainC2.BogeyONE
+local train1A = workspace.TrainC2.BogeyTest
+-----
+local RS = game:GetService("ReplicatedStorage")
+-----
+local lon = RS:WaitForChild("LightsOn") --
+local lof = RS:WaitForChild("LightsOff") --
+local fwd = RS:WaitForChild("Fwd") --
+local rwd = RS:WaitForChild("Rwd") --
+local up = RS:WaitForChild("Up")
+local dn = RS:WaitForChild("Down")
+local eb = RS:WaitForChild("EB") --
+local ho = RS:WaitForChild("Horn") --
+-----
+
+local function playKm() -- plays the km sound
+	workspace.Seat.KM.Playing = true
+	wait(0.425)
+	workspace.Seat.KM.Playing = false
+end
+fwd.OnServerEvent:Connect(playKm)
+rwd.OnServerEvent:Connect(playKm)
+
+local function playLights() -- plays the light tumbler sound
+	workspace.Seat.Lights.Playing = true
+	wait(0.298)
+	workspace.Seat.Lights.Playing = false
+end
+lon.OnServerEvent:Connect(playLights)
+lof.OnServerEvent:Connect(playLights)
