6b44c8d8630f226b387750365eedc2342f37370b
ribzix <37005911+ribzix@users.noreply.github.com>
2021-11-18 21:21:15 +0300

    Update Train.ini.lua

--- Train.ini.lua
+++ Train.ini.lua
@@ -1,4 +1,16 @@
+local train = workspace.Train.BogeyTest
+local trainA = workspace.Train.BogeyONE
+local train1 = workspace.Train.TrainC2.BogeyONE
+local train1A = workspace.Train.TrainC2.BogeyTest
+--
 wait(0.1)
 local n =  math.random(1000, 9999)
-script.Parent.Parent:SetAttribute("number", n) 
-print(script.Parent.Parent:GetAttribute("number"))
+workspace.Train.TRNmbr.SurfaceGui.TextLabel.Text = n
+train:SetAttribute("number", n)
+local n =  math.random(1000, 9999)
+workspace.Train.TrainC2.TRNmbr.SurfaceGui.TextLabel.Text = n
+train1:SetAttribute("number", n)
+--
+print(train:GetAttribute("number"))
+print(train1:GetAttribute("number"))
+
