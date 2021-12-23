0437f8a250aa27c250ae7d8b30ea27fa12644a68
ribzix <37005911+ribzix@users.noreply.github.com>
2021-12-23 23:43:23 +0300

    Update SpeedFormula.lua

--- SpeedFormula.lua
+++ SpeedFormula.lua
@@ -1,7 +1,7 @@
 while true do
 	local part = script.Parent
 	local speed = part.Velocity.magnitude
-	wait(0.01)
+	wait(0.05)
 	speed = speed * 1.003
 	speed = (math.abs(speed))
 	speed = (math.floor(speed))
